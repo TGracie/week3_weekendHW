deb240778db194602b52c9f14f70d0e3750d71ce
Thomas Gracie <tgracie030@gmail.com>
2018-09-28 12:13:54 +0100

    Added CRU to film model, tested and working properly, onto delete

--- models/film.rb
+++ models/film.rb
@@ -15,4 +15,46 @@ class Film
   #####################################################################
   ## CLASS METHODS ##
 
-end
+  def self.delete_all()
+    sql = "DELETE FROM films"
+    SqlRunner.run(sql)
+  end
+  #####################################################################
+  def self.all()
+    sql = "SELECT * FROM films"
+    films = SqlRunner.run(sql)
+    result = films.map{|film| Film.new(film)}
+    return result
+  end
+  #####################################################################
+  #####################################################################
+  ## OBJECT METHODS ##
+
+  def save()
+    sql = "INSERT INTO films
+    (
+      title,
+      price
+      )
+      VALUES($1, $2)
+      RETURNING id"
+      values = [@title, @price]
+    film = SqlRunner.run(sql, values).first
+    @id = film['id'].to_i
+  end
+  #####################################################################
+  def update
+    sql = "UPDATE films
+    SET
+      title = $1,
+      price = $2
+    WHERE id = $3;"
+    values = [@title, @price, @id]
+    SqlRunner.run(sql, values)
+  end
+
+
+#######################################################################
+#######################################################################
+#######################################################################
+end ## CLASS END ##
